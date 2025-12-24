c09c9741b7fc32580baff956c94f20d1b4eacf8f
chintumanohar77-droid <chintumanohar77@gmail.com>
2025-12-24 10:16:50 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        int age=30;
+
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println(age);
+            double price=99.9;
+            System.out.println(price);
+
+            boolean isActive=true;
+            System.out.println(isActive);
+             char grade ='A';
+             System.out.println(grade);
+
+
+
+        }
+    }
